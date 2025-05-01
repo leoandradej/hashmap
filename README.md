ebfe8e96f45dde4e2506e78c51dde0a4b341e1e5
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-05-01 17:52:45 -0300

    Updated README file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# hashmap
+# HashMap and Binary Search Tree
+
+Exercise to practice the implementation of a HashMap and a Binary Search Tree using JavaScript.
--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const HashMap = require("./HashMap");
+const BST = require("./BST");
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
 
 const hm = new HashMap();
 hm.set('Makenzie Palmer', 'Phoenix, AZ')
@@ -14,4 +28,19 @@ hm.set('Ava Mitchell', 'Boston, MA')
 hm.set('Jacob Warren', 'Los Angeles, CA')
 hm.set('Cleo Sawyer', 'New York, NY')
 
-console.log(hm.values())
+//console.log(hm.entries())
+
+const bst = new BST();
+
+bst.add(9);
+bst.add(4);
+bst.add(17);
+bst.add(3);
+bst.add(6);
+bst.add(22);
+bst.add(5);
+bst.add(7);
+bst.add(20);
+bst.add(4);
+
+prettyPrint(bst.root)
